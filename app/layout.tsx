607c01ee4c5afd57f385859faa2af326adc7c5b0
? <rm131700@gmail.com>
2024-09-07 16:14:32 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import './global.css'
+
+function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
+
+export default RootLayout
